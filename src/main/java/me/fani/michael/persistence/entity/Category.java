d66bd8bc4661d9865956cd51c427e8e90cd4552e
Fani-MA <misha.fani501@gmail.com>
2023-05-12 05:11:57 +0300

    add entity from category->product

--- src/main/java/me/fani/michael/persistence/entity/Category.java
+++ src/main/java/me/fani/michael/persistence/entity/Category.java
@@ -1,4 +1,24 @@
 package me.fani.michael.persistence.entity;
 
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
 public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name="NAME")
+    private String name;
+
+    @OneToMany(mappedBy = "category",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @PrimaryKeyJoinColumn
+    private Collection<Product> products;
+
+
+
+
 }
--- src/main/java/me/fani/michael/persistence/entity/Product.java
+++ src/main/java/me/fani/michael/persistence/entity/Product.java
@@ -1,4 +1,24 @@
 package me.fani.michael.persistence.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "PRICE")
+    private double price;
+
+    @Column(name = "CATEGORY")
+    @MapsId
+    @JoinColumn(name ="CATEGORY_ID")
+    @JsonIgnore
+    private Category category;
 }
