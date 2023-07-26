de38f9571d5a466e30b002e59fd53b1a70a1f255
Fani-MA <misha.fani501@gmail.com>
2023-07-26 23:36:12 +0300

    Add users and button buy(cart)

--- src/main/java/me/fani/michael/web/controllers/UserController.java
+++ src/main/java/me/fani/michael/web/controllers/UserController.java
@@ -1,29 +1,33 @@
 package me.fani.michael.web.controllers;
 
 import me.fani.michael.persistence.dao.UserRepo;
+import me.fani.michael.persistence.entity.Role;
 import me.fani.michael.persistence.entity.User;
-import me.fani.michael.web.dto.CreateUserRequest;
-import me.fani.michael.web.dto.Resp;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Connection;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 
 @Controller
-@RequestMapping("users")
+@RequestMapping("user")
 public class UserController {
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @Autowired
     private UserRepo userRepo;
 
 
-
+    //TODO переделать
     @GetMapping
     @PreAuthorize("hasAuthority('user:write')")
     public String allUsers(Model model) {
@@ -32,19 +36,19 @@ public class UserController {
     }
 
     @GetMapping("/new")
-    @PreAuthorize("hasAuthority('user:read')")
     public String newUser(Model model){
+        if(SecurityContextHolder.getContext().getAuthentication().getName()==null) return "redirect:/user";
         model.addAttribute("user", new User());
         return "/users/new.html";
     }
 
-
-    @PostMapping()
-    @PreAuthorize("hasAuthority('user:read')")
-    public String createUser(@ModelAttribute("user") User user,Model model) {
+    @PostMapping("/new")
+    public String createUser(@ModelAttribute("user") User user) {
         user.setCreateTime(Timestamp.valueOf(LocalDateTime.now()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
         userRepo.save(user);
-        return "redirect:users";
+        return "redirect:/category";
     }
 
 
